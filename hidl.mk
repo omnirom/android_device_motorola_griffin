d65af59c7f6b54516e4bf4227c0caa123cb4b8e8
Philipp Vogel <philippwolf86.pw@gmail.com>
2017-09-20 23:15:57 +0200

    griffin: build HIDLs
    Change-Id: I252c33166e921dc1b6c80257b57c7d54aa23ccc8

---
+++ hidl.mk
@@ -0,0 +1,84 @@
+# Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl
+
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl
+
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl
+
+# DRM
+PRODUCT_PACKAGES += \
+		android.hardware.drm@1.0-impl
+
+# Dumpstate
+PRODUCT_PACKAGES += \
+		android.hardware.dumpstate@1.0-service.msm8996
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service
+
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+		android.hardware.gatekeeper@1.0-service
+
+# GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl
+
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+		android.hardware.keymaster@3.0-service
+
+# Lights
+PRODUCT_PACKAGES += \
+		android.hardware.light@2.0-impl
+
+# NFC
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl
+
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-impl
+
+# RenderScript
+PRODUCT_PACKAGES += \
+		android.hardware.renderscript@1.0-impl
+
+# Sensors
+PRODUCT_PACKAGES += \
+		android.hardware.sensors@1.0-impl
+
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+
+# VR
+PRODUCT_PACKAGES += \
+    android.hardware.vr@1.0-impl
+
+# WiFi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service
