79350f38d7cab69bb413f603d85a598d86b44bbf
samstone86 <philippwolf86.pw@gmail.com>
2018-02-07 14:10:56 +0100

    griffin: fix hild.mk
    Change-Id: Iefedae58bfe76d5d4c37ae0c61117c293a58ff66

--- hidl.mk
+++ hidl.mk
@@ -44,11 +44,11 @@ PRODUCT_PACKAGES += \
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
-    android.hardware.gatekeeper@1.0-service    
+    android.hardware.gatekeeper@1.0-service
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl
+    android.hardware.gnss@1.0-impl \
     android.hardware.gnss@1.0-impl-qti \
     android.hardware.gnss@1.0-service-qti \
 
