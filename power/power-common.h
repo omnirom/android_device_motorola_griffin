a6035cb5e0673f07aa6ae23fe22792f18b15cde4
Philipp Vogel <philippwolf86.pw@gmail.com>
2018-02-12 13:18:54 +0100

    griffin: update power hal from marlin sources
    Change-Id: I35fe93a4ff10e297e5aaaa8d243d21789cc54165

--- power/power-common.h
+++ power/power-common.h
@@ -37,7 +37,7 @@
 #define ONDEMAND_GOVERNOR "ondemand"
 #define INTERACTIVE_GOVERNOR "interactive"
 #define MSMDCVS_GOVERNOR "msm-dcvs"
-#define SCHED_GOVERNOR "sched"
+#define SCHED_GOVERNOR "schedutil"
 
 #define HINT_HANDLED (0)
 #define HINT_NONE (-1)
@@ -48,3 +48,6 @@ enum CPU_GOV_CHECK {
     CPU2 = 2,
     CPU3 = 3
 };
+
+#define UNUSED(x) UNUSED_ ## x __attribute__((__unused__))
+
