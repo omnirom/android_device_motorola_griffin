f076ce1088687351cc81000cd0be6340a6fcd784
Philipp Vogel <philippwolf86.pw@gmail.com>
2018-02-13 12:11:12 +0100

    griffin: update BT and build CAF BT
    Change-Id: I7681dcc699c2de3942132503767dd9b2f7e7a9f9

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,6 +1,10 @@
-
 /*
- * Copyright (C) 2016 The CyanogenMod Project <http://www.cyanogenmod.org>
+ *
+ * Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ * Not a Contribution, Apache license notifications and license are retained
+ * for attribution purposes only.
+ *
+ * Copyright (C) 2012 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,13 +22,14 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 #define BTM_DEF_LOCAL_NAME   "Moto Z"
-#define BLUETOOTH_QTI_SW TRUE
+// Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
-#define BLE_VND_INCLUDED   TRUE
-#define BT_CLEAN_TURN_ON_DISABLED 1
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
-#define BLUETOOTH_QCOM_SW TRUE
-#define BLUETOOTH_QCOM_LE_INTL_SCAN TRUE
-#define BTC_INCLUDED TRUE
+// Wide-band speech support
+#define BTM_WBS_INCLUDED TRUE
+#define BTIF_HF_WBS_PREFERRED TRUE
+// Google VSC spec support
+#define BLE_VND_INCLUDED TRUE
+// QCOM power management workaround
+#define BT_CLEAN_TURN_ON_DISABLED TRUE
 #endif
