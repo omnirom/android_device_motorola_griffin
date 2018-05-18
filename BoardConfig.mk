1dbf379ba77ce0a6f6d420dd18f25578271d4652
Philipp Vogel <philippwolf86.pw@gmail.com>
2018-05-18 11:03:10 +0200

    griffin: prepare for O blobs
    Change-Id: I15adad2c62a60885cac2cf50eda497c1d4b3c963

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,7 +173,7 @@ BOARD_USES_QCOM_HARDWARE := true
 BOARD_USES_QC_TIME_SERVICES := true
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.full
+TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.qcom
 
 # Keystore
 TARGET_PROVIDES_KEYMASTER := true
