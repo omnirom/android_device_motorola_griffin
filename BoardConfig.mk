cef2a807b8b7618631a319b85fa5a197ef5d1c9f
Erfan Abdi <erfangplus@gmail.com>
2018-08-19 10:53:28 +0430

    griffin: permissive again
    Change-Id: I3ce34a0bb16b9e9b1287eb89ac813a38420d1330
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,6 +105,7 @@ TARGET_USES_GRALLOC1 := true
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.selinux=enforcing user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += cnsscore.pcie_link_down_panic=1
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
