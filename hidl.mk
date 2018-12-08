53e36a6d58569f286ce9446498ae68a299125d53
Vachounet <vachounet@live.fr>
2018-12-08 15:56:22 +0100

    griffin: use moto camera provider
    Change-Id: Ic6be84b0f98006a52486c7a79eca5525623b66f5

--- hidl.mk
+++ hidl.mk
@@ -14,8 +14,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-impl
 
 # Display
 PRODUCT_PACKAGES += \
