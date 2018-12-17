19f99383d134fd0aab824a008863a6f61f2bb97b
Vachounet <vachounet@live.fr>
2018-12-17 10:29:22 +0100

    griffin: fix bootanim
    Change-Id: Ic169e66c1c7d02cf3a8ad8a3077ed309ee6eab3f

--- omni_griffin.mk
+++ omni_griffin.mk
@@ -22,7 +22,7 @@ $(call inherit-product, vendor/omni/config/gsm.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # must be before including omni part
-TARGET_BOOTANIMATION_SIZE := 1440p
+TARGET_BOOTANIMATION_SIZE := 1080p
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
