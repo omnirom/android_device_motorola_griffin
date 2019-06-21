3896697883b9dab47d729bcba1dcd0c40e4ddbcb
Vache Ounet <vachounet@live.fr>
2019-06-21 09:06:30 +0100

    Revert "griffin: remove ModPermsFix package"
    This reverts commit cda59ac3bb31d6df79497f7bc5101d9b905c9687.
    
    Change-Id: I4b7462c21757467f7dafc3acaaa306712f8a9c9c

--- device.mk
+++ device.mk
@@ -187,6 +187,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Mods
+PRODUCT_PACKAGES += \
+    ModPermsFix
+
 # Net
 PRODUCT_PACKAGES += \
     libandroid_net \
